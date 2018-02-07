4d15fed52d48ccea3476805c63ddd99834b86983
Erik Lindqvist <eriklindqvist@users.noreply.github.com>
2018-02-07 10:48:05 +0100

    Tag image to use private docker repo

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o auth .
-docker build -t auth .
+docker build -t recepies_auth .
+docker tag recepies_auth:latest proto:5000/recepies_auth
